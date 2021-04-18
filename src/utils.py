25149f8994860943aa6b6577a078bd69bacef4bf
lilboiice <armonah2@gmail.com>
2021-04-18 13:35:56 +0000

    added saving function in utils

--- src/utils.py
+++ src/utils.py
@@ -13,4 +13,17 @@ def to_cuda(elements):
         return elements.cuda()
     return elements
 
+def save_result(model, model_name, accuracy, average_dice, class_dice, msg=None):
+    # Save performance
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    with open(MODEL_PATH + "performance.txt", "a") as text_file:
+        print(f"data:{now}\ndataset:{DATA_PARAMS['dataset']}\n"
+              f"epoch:{EPOCHS}\nimage_resolution:{DATA_PARAMS['image_resolution']}\n" 
+              f"pre_proc:{PREP_STEPS}\nacc:{round(accuracy, 4)}\n"
+              f"avg_dice:{round(average_dice, 4)}\nclass_dice_scores:{str(class_dice)}\n", 
+              f"description:{msg}",
+              file = text_file)
 
+    # Save model
+    torch.save(model.state_dict(), MODEL_PATH + model_name+'.pt')
+    print(f"Model state has been saved in {MODEL_PATH+model_name}")
